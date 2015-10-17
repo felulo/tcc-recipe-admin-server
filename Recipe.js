feec16a842beb4cfe12c57623fad4f8e2a45c0a7
Felipe Luis Lopes <felipelopes.luis@gmail.com>
2015-10-17 20:41:38 -0300

    Add Schema Mixed

--- Recipe.js
+++ Recipe.js
@@ -2,6 +2,7 @@
  * Created by felipe.lopes on 10/07/2015.
  */
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 var RecipeSchema = new mongoose.Schema({
   name: String,
@@ -14,7 +15,7 @@ var RecipeSchema = new mongoose.Schema({
       optional: Boolean,
       obs: String
     }],
-    prepare: [String]
+    prepare: [Schema.Types.Mixed]
   }],
   time: {
     hrs: Number,
@@ -25,7 +26,7 @@ var RecipeSchema = new mongoose.Schema({
     count: Number,
     unit: String
   },
-  images: [String]
+  images: [Schema.Types.Mixed]
 });
 
 module.exports = mongoose.model('Recipe', RecipeSchema);
